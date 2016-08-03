1be5fe41080d0ffd03d3caf818d835dbf99ca77d
Nick Pisciotta <npisciotta@gmail.com>
2016-08-03 15:06:38 -0600

    Change logic in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,11 +6,13 @@ class DashboardController < ApplicationController
   def show
     @activity = Activity.new(current_user, params[:date])
     @meals = Meal.find_meals_by_day(current_user, params[:date])
+    @goals = current_user.goals.last
     if @goals.nil?
+      byebug
       flash[:notice] = "Please Add Goals"
       redirect_to new_goal_path
     else
-      @goals = current_user.goals.last
+      @goals
     end
   end
 
