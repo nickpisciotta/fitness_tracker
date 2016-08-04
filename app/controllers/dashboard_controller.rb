a2716d625901b02a7c6cda2ebafc7d2a01224abb
Nick Pisciotta <npisciotta@gmail.com>
2016-08-04 02:31:51 -0600

    fix dashbaord index date dropdown

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,8 @@ class DashboardController < ApplicationController
   end
 
   def show
-    @activity = Activity.new(current_user, params[:date])
-    @meals = Meal.find_meals_by_day(current_user, params[:date])
+    @activity = Activity.new(current_user, params[:date][:Date])
+    @meals = Meal.find_meals_by_day(current_user, params[:date][:Date])
     @goals = current_user.goals.last
     if @goals.nil?
       flash[:notice] = "Please Add Goals"
